bcd76c70fc4f2eb094a8c76d43984d47fbebfe7f
nishat1 <nishatgupta1@gmail.com>
2019-03-25 23:12:59 -0700

    Updates

---
+++ precache-manifest.a1a092ca08ef31fc3f9548e3247f995e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c2d5fae667338081b3d3d9db93150d",
+    "url": "/smartender/static/media/favicon.22c2d5fa.ico"
+  },
+  {
+    "revision": "02b196e7e2b844e14cdd",
+    "url": "/smartender/static/js/runtime~main.8105a4e9.js"
+  },
+  {
+    "revision": "a6a1872ee8d3604160b4",
+    "url": "/smartender/static/js/main.59623811.chunk.js"
+  },
+  {
+    "revision": "800e7198cb9a253350d4",
+    "url": "/smartender/static/js/2.f5e230ba.chunk.js"
+  },
+  {
+    "revision": "a6a1872ee8d3604160b4",
+    "url": "/smartender/static/css/main.304b7164.chunk.css"
+  },
+  {
+    "revision": "b2de7dbcc402a2a4efb504a7d8ccd84d",
+    "url": "/smartender/index.html"
+  }
+];
